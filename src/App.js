08ebf1d42c7c1ec6037ee9f05e6d75de4c635d30
King's meow <mingee641@gmail.com>
2023-07-12 09:04:21 +0900

    조건부 렌더링,주석,className,인라인 스타일링

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import { Fragment } from "react";
-
+import './App.css';
 
 function App() {
+  const name = '리액트';
   return (
     <>
-      <h1>리액트 안녕!</h1> 
-      <h2>잘 작동하니?</h2>
+      {/* 주석은 이렇게 작성한다. */}
+      <div
+        className="react" // 시작 태그를 여러 주로 작성하게 된다면 여기에 주석을 작성할 수 있다.
+      >
+        {name}
+      </div>
+      // 하지만 이런 주석이나 /* 이런 주석은 페이지에 그대로 나타나게 된다 */
+      <input />
     </>
   );
 }
