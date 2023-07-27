86cec08d5c548d3bdaa65e2b7d4d18b57ac3079b
King's meow <mingee641@gmail.com>
2023-07-27 17:39:41 +0900

    8.6 useRef

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
 import Average from "./Average";
-import Counter from "./Counter";
-import Info from "./Info";
+
 
 const App = () => {
-  // return <Counter />;
-  // return <Info />;
   return <Average />
 };
 
