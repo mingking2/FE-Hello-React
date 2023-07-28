a92ef4b8d44fefd8b512bcac7a8e71936e02db45
King's meow <mingee641@gmail.com>
2023-07-29 03:12:05 +0900

    스터디 3주차 과제

--- src/App.js
+++ src/App.js
@@ -1,8 +1,50 @@
-import Average from "./Average";
 
+import { useState } from 'react';
+import { FaPlus, FaTrash } from 'react-icons/fa';
+import useLocalStorage from './useLocalStorage';
+import './App.css';
 
 const App = () => {
-  return <Average />
-};
+  const [memo, setMemo] = useState('');
+  const [memos, setMemos] = useLocalStorage('memos', []);
+
+  const handleAddMemo = () => {
+    if (memo.trim() !== '') {
+      setMemos((prevMemos) => [...prevMemos, { id: Date.now(), text: memo }]);
+      setMemo('');
+    }
+  };
+
+  const handleDeleteMemo = (memoId) => {
+    setMemos((prevMemos) => prevMemos.filter((m) => m.id !== memoId));
+  };
+
+  return (
+    <div className="App">
+      <h1>Memo App</h1>
+      <div>
+        <input
+          type="text"
+          value={memo}
+          onChange={(e) => setMemo(e.target.value)}
+          placeholder="Enter your memo..."
+        />
+        <button onClick={handleAddMemo}>
+          <FaPlus />
+        </button>
+      </div>
+      <ul>
+        {memos.map((m) => (
+          <li key={m.id}>
+            {m.text}
+            <button onClick={() => handleDeleteMemo(m.id)}>
+              <FaTrash />
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
 
 export default App;
