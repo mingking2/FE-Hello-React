7f65e8cb555462c552e08229a4b204b7fd0a34b2
King's meow <mingee641@gmail.com>
2023-08-06 17:46:24 +0900

    [ADD] Axios API와 Firebase 연동

--- src/index.js
+++ src/index.js
@@ -2,13 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import AppWithAxios from './AppWithAxios';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  //<React.StrictMode>
-    <App />
-  //</React.StrictMode>
+
+  <AppWithAxios />
+
 );
 
 // If you want to start measuring performance in your app, pass a function
